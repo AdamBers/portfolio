37bc978798ed9e1b28360297816d22b3a934fd5a
Adam <adam@MacBook-Air-Adam.local>
2023-05-01 21:06:01 +0300

    added navbar and navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./scenes/Navbar";
+import DotGroup from "./scenes/DotGroup";
+import Landing from "./scenes/Landing"
+
 function App() {
-  return <div className="App">1</div>;
+  const [selectedPage, setSelectedPage] = useState("home");
+  const isAboveMedumScreens = useMediaQuery("(min-width:1060px)");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-deep-blue">
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+        isTopOfPage={isTopOfPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full"></div>
+      {isAboveMedumScreens && (
+        <DotGroup
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
+      )}
+      <Landing selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
