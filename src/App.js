1c4e5a7f4d43a6965d332652e7d626819e9268b9
Adam <adam@MacBook-Air-Adam.local>
2023-05-02 23:19:09 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import { useEffect, useState } from "react";
-import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from "./scenes/Navbar";
-import DotGroup from "./scenes/DotGroup";
 import Landing from "./scenes/Landing";
-import LineGradient from "./components/LineGradient";
+import DotGroup from "./scenes/DotGroup";
 import MySkills from "./scenes/MySkills";
+import LineGradient from "./components/LineGradient";
 import Projects from "./scenes/Projects";
-import Testimonials from "./scenes/Testimonials.jsx";
-import Contact from "./scenes/Contact"
+import Contact from "./scenes/Contact";
+import Footer from "./scenes/Footer";
+import useMediaQuery from "./hooks/useMediaQuery";
+import { useEffect, useState } from "react";
+import Testimonials from "./scenes/Testimonials";
+import { motion } from "framer-motion";  
 
-function App() {
+function App() { 
   const [selectedPage, setSelectedPage] = useState("home");
-  const isAboveMedumScreens = useMediaQuery("(min-width:1060px)");
   const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isDesktop = useMediaQuery("(min-width: 1060px)");
 
   useEffect(() => {
     const handleScroll = () => {
+      
       if (window.scrollY === 0) {
         setIsTopOfPage(true);
         setSelectedPage("home");
@@ -29,39 +32,69 @@ function App() {
   return (
     <div className="app bg-deep-blue">
       <Navbar
+        isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
-        isTopOfPage={isTopOfPage}
       />
       <div className="w-5/6 mx-auto md:h-full">
-        {isAboveMedumScreens && (
+        {isDesktop && (
           <DotGroup
             selectedPage={selectedPage}
             setSelectedPage={setSelectedPage}
           />
         )}
-        <Landing setSelectedPage={setSelectedPage} />
-      </div>
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("home")}
+        >
+          <Landing setSelectedPage={setSelectedPage} />
+        </motion.div>
+      </div> 
       <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full">
-        <MySkills />
+      <div className="w-5/6 mx-auto md:h-full ">
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("skills")}
+        > 
+          <MySkills />
+        </motion.div>
       </div>
       <LineGradient />
       <div className="w-5/6 mx-auto">
-        <Projects />
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("projects")}
+        >
+          <Projects />
+        </motion.div>
       </div>
-
       <LineGradient />
-      <div className="w-5/6 mx-auto  md:h-full">
-        <Testimonials />
+      <div className="w-5/6 mx-auto md:h-full">
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("testimonials")}
+        >
+          <Testimonials />
+        </motion.div>
       </div>
-
       <LineGradient />
-      <div className="w-5/6 mx-auto  md:h-full">
-        <Contact />
+      <div className="w-5/6 mx-auto md:h-full">
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("contact")}
+        >
+          <Contact />
+        </motion.div>
       </div>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
